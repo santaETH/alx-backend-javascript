e95fff6d49e91b0ff766e788e2ee00478a9c41de
santaETH <sdamissie@yahoo.com>
2024-08-16 19:49:14 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/**
+ * Retrieves students in a given location.
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieves the sum of ids of a list of students.
+ * @returns {Number}
+ */
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
