2e83d15a73c77579bf97c19954ff77c88d5da8cb
santaETH <sdamissie@yahoo.com>
2024-08-16 19:30:41 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+/**
+ * Retrieves ids from a list of students
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
