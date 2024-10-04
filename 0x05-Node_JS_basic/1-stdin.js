7b0d527a9496d1ef1659136f798c8de392dcfbf1
santaETH <sdamissie@yahoo.com>
2024-10-04 19:12:57 +0000

    node_JS_basic

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ * Display the message Welcome to Holberton School..
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
