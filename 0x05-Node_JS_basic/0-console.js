fa1d226c3e18b28fcd58b16bf9d9341d729b446c
santaETH <sdamissie@yahoo.com>
2024-10-04 19:01:58 +0000

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Execute a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
